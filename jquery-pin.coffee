867592b6861834c88c153a83a08e21aeb6142bb6
Ford Lee <fordlee404@gmail.com>
2015-01-31 11:44:39 +0800

    add a container to wrap element

--- jquery-pin.coffee
+++ jquery-pin.coffee
@@ -1,9 +1,32 @@
 ### global jQuery ###
 do($=jQuery)->
   'use strict'
-
+  
   $.fn.pin = (options)->
     options = options || {}
     $elm = @
+    $container = null
+    # isPinned = false
+
+    do->
+      _width = $elm.outerWidth()
+      _height = $elm.outerHeight()
+
+      $elm.wrap '<div style="display:block;"></div>'
+      $container = $elm.parent()
+    
+    ###
+    refreshContainer = ->
+      _width = $elm.outerWidth()
+      _height = $elm.outerHeight()
+      $container.css 'width',_width
+      $container.css 'height',_height
+      return
+
+    $window = $(window)
+    $window.resize ->
+      if isPinned
+        refreshContainer()
+    ###
 
     return @
--- jquery-pin.js
+++ jquery-pin.js
@@ -3,9 +3,31 @@
 (function($) {
   'use strict';
   return $.fn.pin = function(options) {
-    var $elm;
+    var $container, $elm;
     options = options || {};
     $elm = this;
+    $container = null;
+    (function() {
+      var _height, _width;
+      _width = $elm.outerWidth();
+      _height = $elm.outerHeight();
+      $elm.wrap('<div style="display:block;"></div>');
+      return $container = $elm.parent();
+    })();
+
+    /*
+    refreshContainer = ->
+      _width = $elm.outerWidth()
+      _height = $elm.outerHeight()
+      $container.css 'width',_width
+      $container.css 'height',_height
+      return
+    
+    $window = $(window)
+    $window.resize ->
+      if isPinned
+        refreshContainer()
+     */
     return this;
   };
 })(jQuery);
